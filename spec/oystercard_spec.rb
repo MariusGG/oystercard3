868e9c22d56dd00802be81b078d9c31cba941fca
mariusgg <marius.g.guei@gmail.com>
2018-10-10 14:16:35 +0100

    created oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,12 @@
 require 'oystercard'
 describe Oystercard do
+context 'when initialized' do
+  it 'has a balance of zero' do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq 0
+  end
 
+end
 
 
 end
