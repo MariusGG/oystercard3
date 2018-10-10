1150d2b58112c81d0b226a5464ff8d2d6d56b266
rbbri <robin.s.johnson@gmail.com>
2018-10-10 14:30:34 +0100

    Added top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,6 +6,17 @@ context 'when initialized' do
     expect(oystercard.balance).to eq 0
   end
 
+  it 'tops up the card' do
+    random_number = rand
+    subject.top_up(random_number)
+    expect(subject.balance).to eq random_number
+  end
+  # it 'should also add more money' do
+  #   subject.top_up(90)
+  #   expect(subject.balance).to eq 90
+  #
+  # end
+
 end
 
 
